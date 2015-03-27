f58f142b1dc97e25b98359e40113e1a525cbdd01
Seth Lifland <seth.lifland@yale.edu>
2015-03-27 15:00:24 -0400

    fix logic error in dprocess.sh

--- survey/dprocess.sh
+++ survey/dprocess.sh
@@ -5,9 +5,9 @@ for hostdir in results/*; do
     if [[ $hostdir =~ $re ]]
     then
 	outfile="resultstats/"${BASH_REMATCH[1]}"-detailed.txt"
+	echo "processing "$hostdir"..."
+	python process.py $targets > $outfile
     else
 	echo "Script error; can't extract output directory name"
     fi
-    echo "processing "$hostdir"..."
-    python process.py $targets > $outfile
 done
